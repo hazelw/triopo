2cae06f5f5c644a5a4acd6f5187cbc6dfe79c7c9
Hazel Wright <hazel.wright@live.co.uk>
2019-12-21 20:42:20 +0000

    feat: better models, less complainy django

--- triopo/ticketing/models.py
+++ triopo/ticketing/models.py
@@ -8,9 +8,11 @@ class Ticket(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
     description = models.CharField(max_length=1000)
     regarding_user_id = models.CharField(max_length=8, null=True, blank=True)
-    submitted_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    submitted_by = models.ManyToManyField(User, related_name='submitted_ticket')
     # TODO: what if the User doesn't have an account on the system? What
     # if we want to assign to a team instead?
-    assigned_to = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
-    status = models.CharField(choices=TicketStatus)
+    assigned_to = models.ManyToManyField(User, related_name='assigned_ticket')
+    status = models.CharField(
+        max_length=30,
+        choices=[(status, status.value) for status in TicketStatus])
     # attachments = ???
