7a2fdeb4f24b1d95c2b81f151df280f83d769fdf
Hazel Wright <hazel.wright@live.co.uk>
2019-12-21 22:42:59 +0000

    feat: fast forward an hour of messing about with {python/homebrew/ruby/rvm} versions...

--- triopo/ticketing/admin.py
+++ triopo/ticketing/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import Ticket
 
-
-class TicketAdmin(admin.ModelAdmin):
-    list_display = (
-        'title', 'description', 'regarding_user_id', 'priority', 'status'
-    )
-    # TODO: sort out assigned_to, reported_by
-
-
-admin.site.register(Ticket, TicketAdmin)
+# Register your models here.
