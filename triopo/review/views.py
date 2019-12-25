975bf340c52cfe6ae7c94a6fb6840c0a5b979c31
Hazel Wright <hazel.wright@live.co.uk>
2019-12-25 19:43:49 +0000

    feat: display tickets - perhaps

--- triopo/review/views.py
+++ triopo/review/views.py
@@ -8,11 +8,15 @@ from ticketing.constants import TicketStatus
 
 @login_required(login_url='/login/')
 def index(request):
-    # TODO: serious pagination, filtering by status and priority
-    # this is just one big yikes for now
+    # TODO: serious pagination, filtering by status and priority, assigned_to/
+    # created_by - this is just one big yikes for now
 
     tickets = Ticket.objects.filter(status__in=[
         TicketStatus.NEW, TicketStatus.TRIAGED, TicketStatus.ON_HOLD    
-    ]).order_by('-updated_date')
+    ]).order_by('-updated_at')
 
-    return HttpResponse('Review tickets here')
+    context = {
+        'tickets': tickets
+    }
+
+    return render(request, 'review.html', context)
