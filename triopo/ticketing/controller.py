37ec97bf64da474887e0867a6b42629dd7083494
Hazel Wright <hazel.wright@live.co.uk>
2019-12-26 19:54:45 +0000

    feat: redirect to ticket page when ticket has been created

--- triopo/ticketing/controller.py
+++ triopo/ticketing/controller.py
@@ -2,7 +2,7 @@ from .models import Ticket
 
 
 def build_ticket(creator, title, description, regarding_user_id, priority):
-    Ticket.objects.create(
+    return Ticket.objects.create(
         title=title,
         description=description,
         regarding_user_id=regarding_user_id,
